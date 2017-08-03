5936a5725aad107f8307831c68fa9e68f82a2176
brian.reinke <brian.reinke@viamonstra.com>
2017-08-02 23:34:59 -0500

    Changed product icon to app-specific

---
+++ app/src/main/java/com/androidexample/diceapp2/staticClasses/OtherStatic.java
@@ -0,0 +1,10 @@
+package com.androidexample.diceapp2.staticClasses;
+
+/**
+ * Created by brian.reinke on 8/1/2017.
+ */
+
+public class OtherStatic {
+    public static int previousScreenOrientationPosition=-1;
+    public static int runRecyclerViewListenerOncePerPage = 0;
+}
